3415f27212d543e2a5209066b4dd241a11573333
Viplavraja <viplavraja2@gmail.com>
2022-02-12 02:09:59 +0530

    Shorted chuck norris.

--- CODEGOLF/chuck-norris-codesize.c
+++ CODEGOLF/chuck-norris-codesize.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
-char M[999],*p=M,t[999],l='a',r,s=' ',z='0';
-int y=0,x;
-void main(){scanf("%[^\n]",M);while(*p!='\0'){x=7;while(x-->0){r=48+((*p>>x)&1);if(l!=r){if(y!=0)t[y++]=s;t[y++]=z;if(r=='0')t[y++]=z;t[y++]=s;}t[y++]=z;l=r;}p++;}t[y]='\0';printf("%s\n",t);}
+char M[99],*p=M,t[999],s=' ',z='0';
+int y=0,x,l=2,r;
+main(){scanf("%[^\n]",M);while(*p!='\0'){x=7;while(x-->0){r=((*p>>x)&1);if(l!=r){if(y)t[y++]=s;t[y++]=z;if(!r)t[y++]=z;t[y++]=s;}t[y++]=z;l=r;}p++;}t[y]='\0';printf("%s\n",t);}
