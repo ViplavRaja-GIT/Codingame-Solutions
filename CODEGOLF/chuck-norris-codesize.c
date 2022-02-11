b6c95166514f5a435ede20f157281eea3f6d1df1
Viplavraja <viplavraja2@gmail.com>
2022-02-12 01:33:54 +0530

    chuck norris shorted.

--- CODEGOLF/chuck-norris-codesize.c
+++ CODEGOLF/chuck-norris-codesize.c
@@ -1,16 +1,4 @@
 #include <stdio.h>
-char M[999], *p = M, t[999], c, s = ' ', z = '0';
-int j=0,x,l=0,y=0,k;
-void set() {
-  if(c!='1')M[y++]=z;
-  M[y++]=z;
-  M[y++]=s;
-  while(l-->0)M[y++]=z;
-}
-void main(){
-  scanf("%[^\n]",M);
-  while(*p!='\0'){x=7;while(x-->=0)t[j+6-x]=48+((*p>>x)&1);j+=7;p++;}
-  k=j;t[j]='\0';j=0;c=t[j];
-  while(j<=k){if(c==t[j])l++;else{if(y!=0)M[y++]=s;set();c=t[j];l=1;}j++;}
-  M[y]='\0';printf("%s\n",M);
-}
+char M[999],*p=M,t[999],l='a',r,s=' ',z='0';
+int y=0,x;
+void main(){scanf("%[^\n]",M);while(*p!='\0'){x=7;while(x-->0){r=48+((*p>>x)&1);if(l!=r){if(y!=0)t[y++]=s;t[y++]=z;if(r=='0')t[y++]=z;t[y++]=s;}t[y++]=z;l=r;}p++;}t[y]='\0';printf("%s\n",t);}
